66f3ea976afdda0d48e29fed8912b61d13487711
Jairo Miguel <78458400+jmitsuu@users.noreply.github.com>
2024-01-18 10:58:16 -0300

    Delete src directory

--- src/components/Footer.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <footer class="w-full h-[200px] border-t-2 bg-black"></footer>
-</template>
--- src/http/getUrl.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-const url = import.meta.env.VITE_API_URL
-const instance = axios.create({
-  baseURL:`${url}`,
-})
-
-
-export default instance;
