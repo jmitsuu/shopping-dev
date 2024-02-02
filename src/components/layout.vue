ed3a42781f4e2c1fb3267ec8379f7863fa524070
Jairo Miguel Afonso <Jairo@tracelog.net.br>
2024-02-02 16:42:55 -0300

    90%

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -9,7 +9,7 @@ import Footer from "./Footer.vue"
   </div>
   <div
     class="justify-center w-full flex bg-opacity-10 container mx-auto mt-10 mb-10 border min-h-screen rounded-md p-4 bg-white">
-    <div class="grid xl:grid-cols-4 grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-5">
+    <div class="grid 2xl:grid-cols-4 xl:grid-cols-3  grid-cols-2 md:grid-cols-2  gap-5">
       <slot name="prod"></slot>
     </div>
 
